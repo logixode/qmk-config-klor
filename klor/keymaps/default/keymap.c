97749d7598e464e0810ab3648218f4a04a566f99
logixode <rohmad1230@gmail.com>
2025-09-28 18:51:09 +0700

    update: showing "Logixode" below os icon in OLED

--- klor/keymaps/default/keymap.c
+++ klor/keymaps/default/keymap.c
@@ -380,24 +380,17 @@ void matrix_scan_user(void) {
 void render_os_lock_status(void) {
     static const char PROGMEM sep_v[] = {0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0};
     static const char PROGMEM sep_h1[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
-    static const char PROGMEM sep_h2[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
+    // static const char PROGMEM sep_h2[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
     static const char PROGMEM face_1[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0xE1, 0};  
     static const char PROGMEM face_2[] = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xE1, 0}; 
     static const char PROGMEM os_m_1[] = {0x95, 0x96, 0};
     static const char PROGMEM os_m_2[] = {0xB5, 0xB6, 0};
     static const char PROGMEM os_w_1[] = {0x97, 0x98, 0};
     static const char PROGMEM os_w_2[] = {0xB7, 0xB8, 0};
-    static const char PROGMEM s_lock[] = {0x8F, 0x90, 0};
+    // static const char PROGMEM s_lock[] = {0x8F, 0x90, 0};
     static const char PROGMEM n_lock[] = {0x91, 0x92, 0};
     static const char PROGMEM c_lock[] = {0x93, 0x94, 0};
     static const char PROGMEM b_lock[] = {0xE1, 0xE1, 0};
-    #ifdef AUDIO_ENABLE  
-      static const char PROGMEM aud_en[] = {0xAF, 0xB0, 0};
-      static const char PROGMEM aud_di[] = {0xCF, 0xD0, 0};
-    #endif
-    #ifdef HAPTIC_ENABLE
-      static const char PROGMEM hap_en[] = {0xB1, 0xB2, 0};
-    #endif
 
 // os mode status ────────────────────────────────────────┐
 
@@ -423,55 +416,33 @@ void render_os_lock_status(void) {
     oled_write_P(sep_h1, false);
     oled_write_P(face_2, false);
     oled_write_ln_P(sep_v, false);
-
     
-// lock key layer status ─────────────────────────────────┐
-
+    // Display "Logixode" on the left and lock indicators on the right
+    oled_write("Logixode", false);
+    // oled_write_P(sep_h1, false);
+    
+    // oled_write_P(sep_h2, false);
+    // lock key layer status ─────────────────────────────────┐
+    
     led_t led_usb_state = host_keyboard_led_state();
-
+    
     if (led_usb_state.num_lock) {
-        oled_write_P(n_lock, false); // ──── NUMLOCK
+      oled_write_P(n_lock, false); // ──── NUMLOCK
     } else {
-        oled_write_P(b_lock, false);
+      oled_write_P(b_lock, false);
     }
     if (led_usb_state.caps_lock) {
-        oled_write_P(c_lock, false); // ─── CAPSLOCK
+      oled_write_P(c_lock, false); // ─── CAPSLOCK
     } else {
-        oled_write_P(b_lock, false);
+      oled_write_P(b_lock, false);
     }
-    if (led_usb_state.scroll_lock) { // ─ SCROLLLOCK
-        oled_write_P(s_lock, false);
-    } else {
-        oled_write_P(b_lock, false);
-    }
+  
 
-// hardware feature status ──────────────────────────────┐
-
-    oled_write_P(sep_h2, false);
-
-    #ifndef AUDIO_ENABLE 
-        oled_write_P(b_lock, false);
-    #endif
-    #ifndef HAPTIC_ENABLE 
-        oled_write_P(b_lock, false);
-    #endif
-
-    #ifdef AUDIO_ENABLE // ────────────────── AUDIO
-        if (is_audio_on()) { 
-          oled_write_P(aud_en, false); 
-        } else {
-          oled_write_P(aud_di, false);
-        }
-    #endif // AUDIO ENABLE
-
-     #ifdef HAPTIC_ENABLE // ─────────────── HAPTIC
-        oled_write_P(hap_en, false); 
-     #endif // HAPTIC ENABLE
 }
 
 
-// layer status ──────────────────────────────────────────┐
-
+  // layer status ──────────────────────────────────────────┐
+  
 int layerstate = 0;
 
 layer_state_t layer_state_set_kb(layer_state_t state) {
